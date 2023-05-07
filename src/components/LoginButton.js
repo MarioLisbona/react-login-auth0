22f26f7bd3d938150b9cfb8b3d9b6a56beca0c7c
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-05-07 19:56:21 +1000

    Add .env variables and fix fetch to API

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,30 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  async function embeddedLogin() {
+  const [userEmail, setUserEmail] = useState();
+
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const clientSecret = process.env.REACT_APP_AUTH0_CLIENT_SECRET;
+
+  const data = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    connection: "email",
+    email: userEmail,
+    send: "link",
+    authParams: {
+      scope: "openid",
+    },
+  };
+
+  async function embeddedLogin(data) {
     try {
-      const response = await fetch(
-        "https://dev-fptiqhag41j3zo1q.au.auth0.com/passwordless/start",
-        {
-          method: "post",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: {
-            client_id: "X0EXNQ7st47i39RNyUNGWAUmXNq8aN33",
-            client_secret:
-              "iJ6ksnI37OXZKKxvTSi4A1wma3nIsLFyEGGdc3vaAY0r8O5-JQ3so9s2A_F-5id6",
-            connection: "email",
-            email: "mario.lisbona@gmail.com",
-            send: "link",
-            authParams: {
-              scope: "openid",
-            },
-          },
-        }
-      );
+      const response = await fetch(`https://${domain}/passwordless/start`, {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
       const result = await response.json();
       console.log("Success", result);
@@ -34,7 +39,19 @@ const LoginButton = () => {
   }
 
   return (
-    !isAuthenticated && <button onClick={() => embeddedLogin()}>Sign In</button>
+    <>
+      {!isAuthenticated && (
+        <>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={userEmail}
+            onChange={(evt) => setUserEmail(evt.target.value)}
+          ></input>
+          <button onClick={() => embeddedLogin(data)}>Sign In</button>
+        </>
+      )}
+    </>
   );
 };
 
